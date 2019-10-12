7b462ede6e9d784005ae926fc3a8e5818ff0de99
Chris Birchall <chris.birchall@gmail.com>
2019-10-12 21:29:35 +0100

    Diffuse materials (chapter 8)

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,19 @@ use sphere::Sphere;
 use hittable::Hittable;
 use camera::Camera;
 use rand::Rng;
+use rand::rngs::ThreadRng;
 
 fn main() {
-    raytrace()
+    let rng = rand::thread_rng();
+    raytrace(rng)
 }
 
-fn colour<T: Hittable>(r: Ray, world: &T) -> Vec3 {
-    match world.hit(&r, 0.0, std::f64::MAX) {
+fn colour<T: Hittable>(r: Ray, world: &T, rng: ThreadRng) -> Vec3 {
+    match world.hit(&r, 0.001, std::f64::MAX) {
         Some(hit_record) => {
-            0.5 * Vec3(
-                hit_record.normal.x() + 1.0,
-                hit_record.normal.y() + 1.0,
-                hit_record.normal.z() + 1.0
-            )
+            let target = hit_record.p + hit_record.normal + random_in_unit_sphere(rng);
+            let diffuse_ray = Ray { origin: hit_record.p, direction: target - hit_record.p };
+            0.5 * colour(diffuse_ray, world, rng)
         }
         None => {
             let unit_direction = Vec3::unit_vector(r.direction);
@@ -34,8 +34,20 @@ fn colour<T: Hittable>(r: Ray, world: &T) -> Vec3 {
     }
 }
 
-fn raytrace() {
-    let mut rng = rand::thread_rng();
+fn random_double(mut rng: ThreadRng) -> f64 {
+    rng.gen_range(0.0, 1.0)
+}
+
+fn random_in_unit_sphere(rng: ThreadRng) -> Vec3 {
+    loop {
+        let p = 2.0 * Vec3(random_double(rng), random_double(rng), random_double(rng)) - Vec3(1.0, 1.0, 1.0);
+        if p.squared_length() < 1.0 {
+            return p;
+        }
+    }
+}
+
+fn raytrace(rng: ThreadRng) {
     let nx = 200;
     let ny = 100;
     let ns = 100;
@@ -61,15 +73,17 @@ fn raytrace() {
     let world = vec![sphere, background];
 
     for j in (0..ny).rev() {
+        eprintln!("y = {}", j);
         for i in 0..nx {
             let mut col = Vec3(0.0, 0.0, 0.0);
             for _ in 0..ns {
-                let u = ((i as f64) + rng.gen_range(0.0, 1.0)) / (nx as f64);
-                let v = ((j as f64) + rng.gen_range(0.0, 1.0)) / (ny as f64);
+                let u = ((i as f64) + random_double(rng)) / (nx as f64);
+                let v = ((j as f64) + random_double(rng)) / (ny as f64);
                 let ray = camera.get_ray(u, v);
-                col += colour(ray, &world);
+                col += colour(ray, &world, rng);
             }
             col /= ns as f64;
+            col = Vec3(col.r().sqrt(), col.g().sqrt(), col.b().sqrt());
             let ir = (255.99 * col.r()) as i64;
             let ig = (255.99 * col.g()) as i64;
             let ib = (255.99 * col.b()) as i64;
